d691b52f8db66c9384df801fd2e2c8d94ef871ce
zac432 <zel@torontomu.ca>
2025-12-18 15:48:37 -0500

    resume downloads

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -31,7 +31,7 @@ export const AboutSection = () => {
                                  Get in touch
                             </a>
 
-                            <a className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                            <a href="/Zakaria_El_Arwadi_Data_Analyst_Software_Developer.pdf"  download className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                                 Download CV
                             </a>
 
