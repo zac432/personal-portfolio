e59d4fcb40c19c29e8d83a69b1dfe9b969ea9633
zac432 <zel@torontomu.ca>
2025-12-18 16:31:09 -0500

    show dark themed on mobile

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -32,7 +32,7 @@ export const ThemeToggle = () => {
 }
 
     return (
-        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+        <button onClick={toggleTheme} className={cn("fixed top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
             "focus:outline-hidden"
             )}>
             {" "}
